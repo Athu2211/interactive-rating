6960056f73209ecc3e9bfa2e9aebcf322cbb83a8
nxf87957 <athulya.arokiyasamy@nxp.com>
2022-12-22 13:01:37 +0530

    Interactive rating app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { ReactComponent as Icon } from './images/icon-star.svg'
+import { ReactComponent as Thanks } from './images/illustration-thank-you.svg'
 
 function App() {
+  const rating = [1, 2, 3, 4, 5]
+  const [selectedValue, setSelectedValue] = useState();
+  const [submmittedValue, setSubmittedValue] = useState();
+
+  console.log("Selected value", selectedValue)
+  console.log("Submitted value", submmittedValue)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='rating-container'>
+      <div className='rating-content'>
+        {!submmittedValue ? (
+          <>
+            <div className='icon-container'>
+              <Icon className='icon' />
+            </div>
+            <div className='rating-text'>
+              <h2>How did we do?</h2>
+              <p>Please let us know how we did with your support request. All feedback is appreciated
+                to help us improve our offering!</p>
+            </div>
+            <div className='rating-value'>
+              {rating.map((v) => {
+                return (
+                  <button key={v} className={selectedValue === v ? 'rating-values-selected' : 'rating-values'} onClick={() => { setSelectedValue(v) }}>{v}</button>
+                )
+              })}
+            </div>
+            <button className='submit' onClick={() => setSubmittedValue(selectedValue)}>Submit</button>
+          </>
+        )
+          : (
+            <>
+              <div className='image-container'>
+                <Thanks />
+              </div>
+              <div className='thanks-text'>
+                <div className='selection-text'>You selected {submmittedValue} out of 5</div>
+                <h2>Thank you!</h2>
+                <p>We appreciate you taking the time to give a rating. If you ever need more support,
+                  don’t hesitate to get in touch!</p>
+              </div>
+            </>
+          )}
+      </div>
     </div>
   );
 }
